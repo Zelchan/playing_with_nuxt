daa4e849afce9657d7c6ae3464a7b0cda3666ed7
Andrew Beal <elvithral@gmail.com>
2022-12-25 13:18:13 -0500

    added some basic pages to play with

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container mx-auto w-1/2 py-8">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus fugiat vel repellat qui laborum odio possimus consequuntur, placeat et officia distinctio. Facilis dolorum magni est voluptate nesciunt ut hic ea?
+    </div>
+</template>
